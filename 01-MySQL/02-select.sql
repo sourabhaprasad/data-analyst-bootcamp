dccac7d3b2a7a9a3dee6b4bf12045395d73ffa08
Sourabha K H <sourabhaprasad04@gmail.com>
2024-09-03 11:19:52 +0530

    distinct & pemdas

--- 01-MySQL/02-select.sql
+++ 01-MySQL/02-select.sql
@@ -1,8 +1,22 @@
 SELECT *
 FROM employee_demographics;
 
+SELECT first_name
+FROM employee_demographics;
+
+SELECT first_name, last_name, birth_date
+FROM employee_demographics;
+
 SELECT * 
 FROM employee_salary;
 
 SELECT *
 FROM parks_departments;
+
+-- PEMDAS: Parantheses, Exponential, Multiplication, Division, Addition, Subtraction
+SELECT first_name, last_name, salary, (salary * 1.1)+10000
+FROM employee_salary;
+
+-- Selects DISTINCT values from a column
+SELECT DISTINCT gender
+FROM employee_demographics;
